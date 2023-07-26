c5f1332f81d95b3984f9e64d9acc8af9570c94cf
Ayine-nongre <eugeneatinbire@gmail.com>
2023-07-26 01:27:04 +0000

    Betty correction

--- string_funcs.c
+++ string_funcs.c
@@ -10,23 +10,23 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-        int i = 0, j = 0;
+	int i = 0, j = 0;
 
-        while (*(dest + i) != '\0')
-        {
-                i++;
-        }
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
 
-        while (n > 0)
-        {
-                *(dest + i) = *(src + j);
-                if (*(src + j) == '\0')
-                        break;
-                i++;
-                j++;
-                n--;
-        }
-        return (dest);
+	while (n > 0)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+			break;
+		i++;
+		j++;
+		n--;
+	}
+	return (dest);
 }
 
 /**
@@ -38,14 +38,14 @@ char *_strncat(char *dest, char *src, int n)
 
 int _strcmp(char *s1, char *s2)
 {
-        int i = 0, diff = 0;
+	int i = 0, diff = 0;
 
-        while (diff == 0)
-        {
-                if (*(s1 + i) == '\0' && *(s2 + i) == '\0')
-                        break;
-                diff = (*(s1 + i) - *(s2 + i));
-                i++;
-        }
-        return (diff);
+	while (diff == 0)
+	{
+		if (*(s1 + i) == '\0' && *(s2 + i) == '\0')
+			break;
+		diff = (*(s1 + i) - *(s2 + i));
+		i++;
+	}
+	return (diff);
 }
